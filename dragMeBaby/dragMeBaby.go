d9fc73c87c50f64b22eefd1114c939980504c4b4
K1tig <m603holden@gmail.com>
2024-06-25 21:54:54 -0400

    sorted action switch out a little

--- dragMeBaby/dragMeBaby.go
+++ dragMeBaby/dragMeBaby.go
@@ -1,180 +1,129 @@
 package main
 
+// A simple example that shows how to send activity to Bubble Tea in real-time
+// through a channel.
+
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 
-	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/bubbles/timer"
+	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-var staging float32
+// A message used to indicate that activity has occurred. In the real world (for
+// example, chat) this would contain actual data.
+type responseMsg struct{}
 
-const prestage = 10
-const fullstage = 7
-const raceyellow = 1.3
-const racegreen = .4
+type keyMap struct {
+	Action key.Binding
+	Quit   key.Binding
+}
 
-const runprogram = true
+var keys = keyMap{
+	Action: key.NewBinding(
+		key.WithKeys("g"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("q", "Q"),
+	),
+}
 
-func racetime(stage int) {
-	p1 := time.Duration(rand.Intn(stage+1) + 1)
-	time.Sleep(p1 * time.Second)
+// Simulate a process that sends events at an irregular interval in real time.
+// In this case, we'll send events on the channel at a random interval between
+// 100 to 1000 milliseconds. As a command, Bubble Tea will run this
+// asynchronously.
+func listenForActivity(sub chan struct{}) tea.Cmd {
+	return func() tea.Msg {
+		for {
+			time.Sleep(time.Millisecond * time.Duration(rand.Int63n(900)+100)) // nolint:gosec
+			sub <- struct{}{}
+		}
+	}
+}
+
+// A command that waits for the activity on a channel.
+func waitForActivity(sub chan struct{}) tea.Cmd {
+	return func() tea.Msg {
+		return responseMsg(<-sub)
+	}
 }
 
 type model struct {
-	timer    timer.Model
-	keymap   keymap
-	help     help.Model
-	quitting bool
-	rStatus  raceStatus
-}
-
-type raceStatus struct {
-	begin    bool
-	preStage bool
-	stage    bool
-	yellow   bool
-	green    bool
-	end      bool
-}
-
-type keymap struct {
-	start key.Binding
-	stop  key.Binding
-	quit  key.Binding
+	levels    int
+	sub       chan struct{} // where we'll receive activity notifications
+	responses int           // how many responses we've received
+	spinner   spinner.Model
+	keys      keyMap
+	quitting  bool
 }
 
 func (m model) Init() tea.Cmd {
-	return m.timer.Init()
+	return tea.Batch(
+		m.spinner.Tick,
+		listenForActivity(m.sub), // generate activity
+		waitForActivity(m.sub),   // wait for activity
+	)
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case timer.TickMsg:
-		var cmd tea.Cmd
-		m.timer, cmd = m.timer.Update(msg)
-		return m, cmd
-
-	case timer.StartStopMsg:
-		var cmd tea.Cmd
-		m.timer, cmd = m.timer.Update(msg)
-		m.keymap.stop.SetEnabled(m.timer.Running())
-		m.keymap.start.SetEnabled(!m.timer.Running())
-		return m, cmd
-
-	case timer.TimeoutMsg:
-		m.quitting = true
-		return m, tea.Quit
-
 	case tea.KeyMsg:
 		switch {
-		case key.Matches(msg, m.keymap.quit):
+		case key.Matches(msg, m.keys.Quit):
 			m.quitting = true
 			return m, tea.Quit
-
-		case key.Matches(msg, m.keymap.start):
+		case key.Matches(msg, m.keys.Action): // location for action input / multi button
 			switch {
-			case m.rStatus.begin != true:
-				m.rStatus.begin = true
-				racetime(prestage)
-				return m, nil
-
-			case m.rStatus.preStage != true:
-				m.rStatus.preStage = true
-				racetime(fullstage)
-				return m, nil
-
-			case m.rStatus.yellow != true:
-				m.rStatus.yellow = true
-				time.Sleep((raceyellow * 1000) * time.Millisecond)
-				return m, nil
-
-			case m.rStatus.green != true:
-				m.rStatus.green = true
-				time.Sleep((racegreen * 1000) * time.Millisecond)
-				return m, nil // Send CMD to start timer. Above Code shouldnt be bound to start key.
-
+			case m.levels == 0:
+				//time.Sleep(time.Second * 4)
+				m.levels++
+			case m.levels == 1: // prestage
+				m.levels++
+			case m.levels == 2: // stage
+				m.levels++
+			case m.levels > -3:
+				m.levels += 2
 			}
-			return m, m.timer.Toggle()
-		}
-	}
 
+			return m, nil
+		}
+
+	case responseMsg:
+		m.responses++                    // record external activity
+		return m, waitForActivity(m.sub) // wait for next event
+	case spinner.TickMsg:
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	default:
+		return m, nil
+	}
 	return m, nil
 }
 
-func (m model) helpView() string {
-	return "\n" + m.help.ShortHelpView([]key.Binding{
-		m.keymap.start,
-		m.keymap.stop,
-
-		m.keymap.quit,
-	})
-}
-
 func (m model) View() string {
-	// For a more detailed timer view you could read m.timer.Timeout to get
-	// the remaining time as a time.Duration and skip calling m.timer.View()
-	// entirely.
-	s := m.timer.View()
-
-	if m.timer.Timedout() {
-		s = "All done!"
-	}
-	s += "\n"
-	if !m.quitting {
-		s = "Exiting in " + s
-		s += m.helpView()
+	s := fmt.Sprintf("\n %s Events received: %d\n\n Press any key to exit\n\n Level of stage: %d", m.spinner.View(), m.responses, m.levels)
+	if m.quitting {
+		s += "\n"
 	}
 	return s
 }
 
 func main() {
-	r1 := raceStatus{
-		false, false, false, false,
+	p := tea.NewProgram(model{
+		sub:     make(chan struct{}),
+		spinner: spinner.New(),
+		keys:    keys,
+		levels:  1,
+	})
+
+	if _, err := p.Run(); err != nil {
+
+		fmt.Println("could not start program:", err)
+		os.Exit(1)
 	}
-	fmt.Printf("Staring Race\n")
-	for runprogram == true {
-
-		r1.current = true
-		// Press start key
-
-		r1.foulOn = true
-		for r1.foulOn {
-
-			p1 := time.Duration(rand.Intn(prestage+1) + 1)
-			time.Sleep(p1 * time.Second)
-			fmt.Println("Prestage... ")
-
-			s1 := time.Duration(rand.Intn(fullstage+1) + 1)
-			time.Sleep(s1 * time.Second)
-			fmt.Printf("Fullstage...\n")
-
-			time.Sleep(time.Duration(raceyellow*1000) * time.Millisecond)
-			fmt.Printf("oo|oo\noo|oo\n 0|0\n 0|0\n 0|0\n")
-
-			time.Sleep(time.Duration(racegreen*1000) * time.Millisecond)
-			fmt.Println(" X|X")
-
-			r1.foulOn = false
-			r1.greenlight = true
-		}
-
-		for r1.greenlight {
-			race1 := time.Duration(rand.Intn(100+1) + 20)
-			time.Sleep((race1 * time.Millisecond))
-			race2 := float32(race1)
-			fmt.Printf("Race Results: 0.%g\n ", race2)
-			r1.greenlight = false
-
-		}
-
-		r1.endrace = true
-		fmt.Printf("End Race\n\n")
-		time.Sleep(5 * time.Second)
-	}
-
 }
