421a86b97f6af8f3851abce46ec29012a41abc13
K1tig <m603holden@gmail.com>
2024-07-01 21:34:49 -0400

    sorted

--- testbin2/main.go
+++ testbin2/main.go
@@ -4,55 +4,35 @@ package main
 // https://github.com/charmbracelet/bubbletea/blob/master/examples/realtime/main.go
 import (
 	"fmt"
-	"math/rand"
 	"os"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
-	sub        chan struct{}
-	keys       keyMap
-	help       help.Model
-	inputStyle lipgloss.Style
-	itemStyle  lipgloss.Style
-	quitting   bool
-	pos        int
-	stg        int
-	stgTimes   stgTimelst
+	sub      chan struct{}
+	keys     keyMap
+	help     help.Model
+	quitting bool
+	stg      int
+	stgT     times
 
 	//	station    stations
 }
 
-type stgTimelst struct {
-	prestg int
-	stg    int
-	yellow int // ,7 - 1.3 sec
-	green  float32
-	active bool
+type times struct {
+	preStg  int
+	fullStg int
+	Yellow  float32
+	Green   float32
 }
 
 type keyMap struct {
 	Twenty key.Binding
 	Quit   key.Binding
-	Help   key.Binding
-}
-
-func (k keyMap) ShortHelp() []key.Binding {
-	return []key.Binding{k.Help, k.Quit}
-}
-
-func (k keyMap) FullHelp() [][]key.Binding {
-	return [][]key.Binding{
-		{k.Twenty},       // first column
-		{k.Help, k.Quit}, // second column
-	}
 }
 
 var keys = keyMap{
@@ -60,80 +40,27 @@ var keys = keyMap{
 		key.WithKeys("g"),
 		key.WithHelp("(g)", "Action"),
 	),
+	Quit: key.NewBinding(
+		key.WithKeys("q"),
+		key.WithHelp("(q)", "Quit"),
+	),
 }
 
 func newModel() model {
 	return model{
-		sub:        make(chan struct{}),
-		keys:       keys,
-		help:       help.New(),
-		inputStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("6")),
-		itemStyle:  lipgloss.NewStyle().Foreground(lipgloss.Color("3")),
-		stgTimes:   stgTimelst{green: .400, active: false},
-		stg:        0,
-		pos:        1,
+		sub:  make(chan struct{}),
+		keys: keys,
+		help: help.New(),
+		stg:  0,
+		stgT: times{preStg: 4, fullStg: 3, Yellow: 1.2, Green: .400},
 	}
 }
 func (m model) Init() tea.Cmd {
 	return tea.Batch(
 		listenForActivity(m.sub), // generate activity
 		waitForActivity(m.sub),   // wait for activity
+
 	)
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-
-	//This needs to be done right so that there is a constant running even that sends a message when changed.
-	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		// If we set a width on the help menu it can gracefully truncate
-		// its view as needed.
-		m.help.Width = msg.Width
-
-	case tea.KeyMsg:
-		switch {
-		case key.Matches(msg, m.keys.Twenty):
-			if !m.stgTimes.active {
-				m.setTimes()
-			}
-		case key.Matches(msg, m.keys.Help):
-			m.help.ShowAll = !m.help.ShowAll
-		case key.Matches(msg, m.keys.Quit):
-			m.quitting = true
-			return m, tea.Quit
-		}
-		return m, waitForActivity(m.sub)
-	case responseMsg:
-		switch m.stgTimes.active {
-		case m.stg != m.pos:
-			switch {
-			case m.stg == 0:
-				stgDelay(m.stgTimes.prestg)
-				m.stg++
-			case m.stg == 1:
-				stgDelay(m.stgTimes.stg)
-				m.stg++
-			case m.stg == 2:
-				stgDelay(m.stgTimes.yellow)
-				m.stg++
-			case m.stg == 3:
-				stgDelay(int(m.stgTimes.green))
-				m.stg++
-			case m.stg == 4:
-				m.stg = 0
-				m.pos = 1
-				m.stgTimes.active = false
-
-			}
-			return m, waitForActivity(m.sub)
-
-		case m.stg == m.pos:
-			m.pos++
-
-		}
-		return m, waitForActivity(m.sub)
-	}
-	return m, waitForActivity(m.sub)
 }
 
 type responseMsg struct{}
@@ -151,41 +78,52 @@ func waitForActivity(sub chan struct{}) tea.Cmd {
 		return responseMsg(<-sub)
 	}
 }
-
-func stgDelay(x int) {
-	time.Sleep(time.Second * time.Duration(x))
-}
-
-// presets for stage times on drag tree
-var stg1 = 6
-var stg2 = 3
-var stg3 = 130
-
-func (m model) setTimes() tea.Model {
-	s := m.stgTimes
-	s.prestg = rand.Intn(stg1-4) + 4
-	s.stg = rand.Intn(stg2-1) + 1
-	s.yellow = (rand.Intn(stg3-70) + 70) / 10
-	s.active = true
-	return m
-}
-
 func (m model) View() string {
-	var status string
-	var stg string
+	return fmt.Sprintf("Current stage:%d", m.stg)
+}
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
-	if m.quitting {
-		return "Bye!\n"
+	//This needs to be done right so that there is a constant running even that sends a message when changed.
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		// If we set a width on the help menu it can gracefully truncate
+		// its view as needed.
+		m.help.Width = msg.Width
 
-	} else {
-		i := m.stg
-		stg = strconv.Itoa(i)
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, m.keys.Quit):
+			m.quitting = true
+			return m, tea.Quit
+		}
+		return m, waitForActivity(m.sub)
+	case responseMsg:
+		switch {
+		case m.stg == 0:
+			time.Sleep(time.Second * time.Duration(m.stgT.preStg))
+			m.stg++
+			return m, waitForActivity(m.sub)
+		case m.stg == 1:
+			time.Sleep(time.Second * time.Duration(m.stgT.fullStg))
+			m.stg++
+			return m, waitForActivity(m.sub)
+		case m.stg == 2:
+			time.Sleep(time.Millisecond * time.Duration(m.stgT.Yellow*1000))
+			m.stg++
+			return m, waitForActivity(m.sub)
+		case m.stg == 3:
+			time.Sleep(time.Millisecond * time.Duration(m.stgT.Green*1000))
+			m.stg++
+			return m, waitForActivity(m.sub)
+		case m.stg == 4:
+			time.Sleep(time.Second * time.Duration(5))
+			m.stg = 0
+			return m, waitForActivity(m.sub)
+		}
 
-		status = m.inputStyle.Render("Stage:") + m.inputStyle.Render(stg)
+		return m, waitForActivity(m.sub)
 	}
-	helpView := m.help.View(m.keys)
-	height := 8 - strings.Count(status, "\n") - strings.Count(helpView, "\n")
-	return "\n" + status + "\n" + strings.Repeat("\n", height) + helpView
+	return m, waitForActivity(m.sub)
 }
 
 // Main Function
